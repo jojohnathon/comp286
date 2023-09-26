321ecf049fc2235d990b6109a498e6023814a209
jojohnathon <pixelizedjohnny@gmail.com>
2023-09-25 22:25:01 -0700

    start cleanString

--- Assignment 3 - String Recursion/src/StringHelper.java
+++ Assignment 3 - String Recursion/src/StringHelper.java
@@ -1,20 +1,40 @@
 public class StringHelper {
     public static void main(String[] args) throws Exception {
-        System.out.println("");
-        cleanString("null");
-        System.out.println("");
-        cleanString("null");
+        String test1 = "aaabbbaccddee";
+        String test2 = "Hello, World!";
+        System.out.println(test1);
+        System.out.println(cleanString(test1));
+        System.out.println(test2);
+        System.out.println(cleanString(test2));
     }
 
     //recursive method that removes duplicate letters; keeps punctuation the same
     public static String cleanString(String input) {
+        /*
+         * call checkchar using charAt()
+         * if true: recurse (input.substring())
+         */
 
-        return input;
+        //base case
+        if (input.length() <= 1) {
+            return input;
+        }
+
+        char currentChar = input.charAt(0);
+        char nextChar = input.charAt(1);
+
+        if (checkChar(currentChar) && (currentChar == nextChar)) { //check for punctuation  //check next character if same
+            return cleanString(input.substring(2));
+        } else {
+            return currentChar + cleanString(input.substring(1));
+        }
     }
 
     //helper method to check if the character is a punctuation
     public static boolean checkChar(char a) {
-
+        if (((a >= 65) && (a <= 90)) || ((a >= 97) && (a <= 122))) { //check ascii table to see if letter
+            return true;
+        }
         return false;
     }
 }
