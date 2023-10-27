d25ed0a52e8fb2534ed5d7cd70bf96eb128d51eb
jojohnathon <pixelizedjohnny@gmail.com>
2023-10-26 18:57:12 -0700

    remove unused imports

--- traffic-data/src/main/java/Main.java
+++ traffic-data/src/main/java/Main.java
@@ -1,32 +1,14 @@
 
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
-import java.util.Scanner;
-import java.util.stream.Collectors;
-
 import javax.ws.rs.core.Response;
 
-import com.opencsv.CSVReaderBuilder;
 import com.opencsv.bean.CsvToBeanBuilder;
 import com.socrata.api.HttpLowLevel;
 import com.socrata.api.Soda2Consumer;
 import com.socrata.model.soql.SoqlQuery;
-
-import au.com.bytecode.opencsv.CSVReader;
-import io.github.cdimascio.dotenv.Dotenv;
 
 public class Main {
     public static void main(String[] args) {
